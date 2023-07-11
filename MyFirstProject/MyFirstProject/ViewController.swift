e5c683c38a8f0382a88f80b6e795a06c1b079f09
Shohzod <shohzodrajabovmir@gmail.com>
2023-07-11 19:34:20 +0500

    -myFirstCommit

---
+++ MyFirstProject/MyFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstProject
+//
+//  Created by Shohzod Rajabov on 11/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
